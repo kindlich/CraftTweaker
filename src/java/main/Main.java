d2ab26c876c110c97afa34bdf729d72e256b2bf2
kindlich <kindlich@gmx.net>
2019-06-16 13:39:23 +0200

    Testing ZC as submodule

---
+++ src/java/main/Main.java
@@ -0,0 +1,12 @@
+package java.main;
+
+//import org.openzen.zencode.java.ScriptingEngine;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //TODO check that this works, either we need to add all
+        // We either need to add all deps in CrT's build.gradle or chagne the JavaScripting deps to actual ones
+        //new ScriptingEngine();
+    }
+}
